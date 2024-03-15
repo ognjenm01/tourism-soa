77457adf1572c347162cf2c43a94e44d0dfbd0e1
Ognjen <ognjenmilojevic2001@gmail.com>
2024-03-15 01:48:48 +0100

    feat: covered tour service with tests

--- module_tour/service/tour_service_test.go
+++ module_tour/service/tour_service_test.go
@@ -10,20 +10,116 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitDB() *gorm.DB {
+func initDB() *gorm.DB {
 	db := test_infrastructure.InitDB()
-	test_infrastructure.ClearDB(db)
 	return db
+}
+
+func initService() TourService {
+	return TourService{TourRepository: &repo.TourRepository{DatabaseConnection: initDB()}}
 }
 
 // Testing insert for tour
 func TestCreateTour(t *testing.T) {
-	tourService := TourService{TourRepository: &repo.TourRepository{DatabaseConnection: InitDB()}}
+	tourService := initService()
 	tour := model.Tour{ID: -1, UserId: -1, Name: "Debug tour", Description: "Debug test here", Price: 300.0, Difficulty: 1, TransportType: 1, Status: 1, Tags: []model.TourTag{}, Keypoints: []model.Keypoint{}, Duration: 5, Distance: 5, StatusUpdateTime: time.Date(
 		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)}
 
 	error := tourService.CreateTour(&tour)
+
 	if error != nil {
 		t.Fatal("Tour insert failed")
 	}
+}
+
+// Testing getting all tours
+func TestGetAll(t *testing.T) {
+	tourService := initService()
+
+	tours, error := tourService.GetAll()
+
+	if error != nil {
+		t.Fatalf("Getting all tours failed: %s", error)
+	}
+
+	if len(*tours) != 3 {
+		t.Fatal("Wrong count of tours in db")
+	}
+}
+
+// Testing getting tours by author
+func TestGetToursByAuthor(t *testing.T) {
+	tourService := initService()
+
+	tours, error := tourService.GetToursByAuthor("-1")
+
+	if error != nil {
+		t.Fatalf("Getting tour by author id: %s", error)
+	}
+
+	if len(*tours) != 2 {
+		t.Fatal("Wrong tour count")
+	}
+
+	if (*tours)[0].Name != "Tour to Spain" || (*tours)[1].Name != "Tour to Argentina" {
+		t.Fatal("Wrong tours found")
+	}
+}
+
+// Testing getting tour by statuses, example {PUBLISHED, DRAFT} as {0, 1}
+func TestGetToursByStatuses(t *testing.T) {
+	tourService := initService()
+
+	tours, error := tourService.GetToursByStatus([]model.TourStatus{0})
+
+	if error != nil {
+		t.Fatalf("Error while getting tour by statuses %s", error)
+	}
+
+	if len(*tours) != 2 {
+		t.Fatal("Wrong tour count")
+	}
+}
+
+// Testing get tour by id
+func TestGetTourById(t *testing.T) {
+	tourService := initService()
+
+	tour, error := tourService.GetTourById("1")
+
+	if error != nil {
+		t.Fatalf("Getting tour by id failed: %s", error)
+	}
+
+	if tour.Name != "Fantastic tour to Serbia" {
+		t.Fatalf("Wrong name, expected 'Fantastic tour to Serbia', got %s", tour.Name)
+	}
+
+	if tour.Price != 300 {
+		t.Fatalf("Wrong price, expected 300, got %f", tour.Price)
+	}
+}
+
+func TestUpdateTour(t *testing.T) {
+	tourService := initService()
+	tour, error := tourService.GetTourById("1")
+
+	if error != nil {
+		t.Fatalf("Failed getting tour by id %s", error)
+	}
+
+	tour.Name = "Fantastic tour to Korea"
+
+	tourService.UpdateTour(1, tour)
+
+	tour, error = tourService.GetTourById("1")
+
+	if error != nil {
+		t.Fatalf("Failed getting tour by id %s", error)
+	}
+
+	if tour.Name != "Fantastic tour to Korea" {
+		t.Fatalf("Expected tour name 'Fantastic tour to Korea', got %s", tour.Name)
+	}
+
 }
--- module_tour/test_infrastructure/test_infrastructure.go
+++ module_tour/test_infrastructure/test_infrastructure.go
@@ -2,7 +2,6 @@ package test_infrastructure
 
 import (
 	"fmt"
-	"os"
 	"tour/model"
 
 	"gorm.io/driver/postgres"
@@ -11,35 +10,11 @@ import (
 )
 
 func createConnectionString() string {
-	/*err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
-	}*/
-
-	host := os.Getenv("HOST")
-	if host == "" {
-		host = "localhost"
-	}
-
-	username := os.Getenv("DB_USERNAME")
-	if username == "" {
-		username = "postgres"
-	}
-
-	password := os.Getenv("DB_PASSWORD")
-	if password == "" {
-		password = "super"
-	}
-
-	dbname := os.Getenv("DB")
-	if dbname == "" {
-		dbname = "explorer-v1"
-	}
-
-	port := os.Getenv("DB_PORT")
-	if port == "" {
-		port = "5432"
-	}
+	host := "localhost"
+	username := "postgres"
+	password := "super"
+	dbname := "tour-test"
+	port := "5432"
 
 	connectionStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, dbname, port)
 	return connectionStr
@@ -65,11 +40,18 @@ func InitDB() *gorm.DB {
 		return nil
 	}
 
+	database.AutoMigrate(&model.Tour{})
+	database.AutoMigrate(&model.TourTag{})
+	database.AutoMigrate(&model.Keypoint{})
+	database.AutoMigrate(&model.TourReview{})
+	database.AutoMigrate(&model.ImageLink{})
+
+	clearDB(database)
 	fillDB(database)
 	return database
 }
 
-func ClearDB(database *gorm.DB) {
+func clearDB(database *gorm.DB) {
 	database.Exec("TRUNCATE tours.tours RESTART IDENTITY CASCADE;")
 	database.Exec("TRUNCATE tours.tour_reviews RESTART IDENTITY CASCADE;")
 	database.Exec("TRUNCATE tours.keypoints RESTART IDENTITY CASCADE;")
@@ -78,20 +60,9 @@ func ClearDB(database *gorm.DB) {
 }
 
 func fillDB(database *gorm.DB) {
-	database.Exec("TRUNCATE tours.tours RESTART IDENTITY CASCADE;")
-	database.Exec("TRUNCATE tours.tour_reviews RESTART IDENTITY CASCADE;")
-	database.Exec("TRUNCATE tours.keypoints RESTART IDENTITY CASCADE;")
-	database.Exec("TRUNCATE tours.tour_tags RESTART IDENTITY CASCADE;")
-
-	database.AutoMigrate(&model.Tour{})
 	database.Exec("INSERT INTO tours.tours(user_id, name, description, price, difficulty, transport_type, status, duration, distance, status_update_time) VALUES ('1', 'Fantastic tour to Serbia', 'Exceptional', '300', '0', '0', '0', '300', '300', '2004-10-19 10:23:54+02');")
-	database.Exec("INSERT INTO tours.tours(user_id, name, description, price, difficulty, transport_type, status, duration, distance, status_update_time) VALUES ('1', 'Tour to Spain', 'Fenomenal', '400', '1', '0', '1', '300', '300', '2004-10-20 11:23:54+02');")
-	database.Exec("INSERT INTO tours.tours(user_id, name, description, price, difficulty, transport_type, status, duration, distance, status_update_time) VALUES ('1', 'Tour to Argentina', 'Wonderful', '500', '1', '2', '0', '300', '300', '2004-10-21 12:23:54+02');")
+	database.Exec("INSERT INTO tours.tours(user_id, name, description, price, difficulty, transport_type, status, duration, distance, status_update_time) VALUES ('-1', 'Tour to Spain', 'Fenomenal', '400', '1', '0', '1', '300', '300', '2004-10-20 11:23:54+02');")
+	database.Exec("INSERT INTO tours.tours(user_id, name, description, price, difficulty, transport_type, status, duration, distance, status_update_time) VALUES ('-1', 'Tour to Argentina', 'Wonderful', '500', '1', '2', '0', '300', '300', '2004-10-21 12:23:54+02');")
 
-	database.AutoMigrate(&model.TourTag{})
 	database.Exec("INSERT INTO tours.tour_tags(tour_id, tag) VALUES ('1', 'Hot');")
-
-	database.AutoMigrate(&model.Keypoint{})
-	database.AutoMigrate(&model.TourReview{})
-	database.AutoMigrate(&model.ImageLink{})
 }
