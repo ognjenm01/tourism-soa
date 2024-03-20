3f3b37a69e09cc4c8df85ccea46dc580c4a34a41
Ljubisa-Peric <peric.ljubo01@gmail.com>
2024-03-20 21:46:35 +0100

    feat: fixed api-microservice communication

--- tourism_monolith/src/Explorer.API/Controllers/Tourist/Commenting/BlogCommentController.cs
+++ tourism_monolith/src/Explorer.API/Controllers/Tourist/Commenting/BlogCommentController.cs
@@ -2,8 +2,11 @@
 using Explorer.Blog.API.Public.Commenting;
 using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Stakeholders.Infrastructure.Authentication;
+using FluentResults;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace Explorer.API.Controllers.Tourist.Commenting;
 
@@ -12,46 +15,108 @@ namespace Explorer.API.Controllers.Tourist.Commenting;
 public class BlogCommentController : BaseApiController
 {
     private readonly IBlogCommentService _blogCommentService;
+    private readonly HttpClient _httpClient;
 
     public BlogCommentController(IBlogCommentService blogCommentService)
     {
         _blogCommentService = blogCommentService;
+        _httpClient = new HttpClient();
     }
 
     [HttpGet]
-    public ActionResult<PagedResult<BlogCommentDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize,
-        [FromQuery] long blogId)
+     public async Task<ActionResult<PagedResult<BlogCommentDto>>> GetAll([FromQuery] int page, [FromQuery] int pageSize, [FromQuery] long blogId)
     {
-        var result = _blogCommentService.GetPaged(page, pageSize, blogId);
-        return CreateResponse(result);
+        Uri uri = new Uri($"http://localhost:3333/api/blogcomments");
+        HttpResponseMessage response = await _httpClient.GetAsync(uri);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseContent = await response.Content.ReadAsStringAsync();
+            List<BlogCommentDto> blogComments = JsonConvert.DeserializeObject<List<BlogCommentDto>>(responseContent);
+            
+            var pagedResult = new PagedResult<BlogCommentDto>(blogComments, totalCount: blogComments.Count);
+
+            return CreateResponse(Result.Ok(pagedResult));
+        }
+
+        return BadRequest();
     }
 
     [HttpGet("{id:int}")]
-    public ActionResult<BlogCommentDto> Get(int id)
+     public async Task<ActionResult<BlogCommentDto>> Get(int id)
     {
-        var result = _blogCommentService.Get(id, User.PersonId());
-        return CreateResponse(result);
-    }
+        Uri uri = new Uri($"http://localhost:3333/api/blogcomments/{id}");
+        HttpResponseMessage response = await _httpClient.GetAsync(uri);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseContent = await response.Content.ReadAsStringAsync();
+            BlogCommentDto blogComment = JsonConvert.DeserializeObject<BlogCommentDto>(responseContent);
+            return CreateResponse(Result.Ok(blogComment));
+        }
+
+        return BadRequest();
+    }  
+    
 
     [HttpPost]
-    public ActionResult<BlogCommentDto> Create([FromBody] BlogCommentDto comment)
+     public async Task<ActionResult<BlogCommentDto>> Create([FromBody] BlogCommentDto blogComment)
     {
-        comment.UserId = User.PersonId();
-        var result = _blogCommentService.Create(comment);
-        return CreateResponse(result);
+        string jsonBlogComment = JsonConvert.SerializeObject(blogComment);
+
+        Uri uri = new Uri("http://localhost:3333/api/blogcomments");
+
+        var content = new StringContent(jsonBlogComment);
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        HttpResponseMessage response = await _httpClient.PostAsync(uri, content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseContent = await response.Content.ReadAsStringAsync();
+            
+            BlogCommentDto createdBlogComment = JsonConvert.DeserializeObject<BlogCommentDto>(responseContent);
+
+            return CreateResponse(Result.Ok(createdBlogComment));
+        }
+        else
+        {
+            return BadRequest();
+        }
     }
 
     [HttpPut("{id:int}")]
-    public ActionResult<BlogCommentDto> Update([FromBody] BlogCommentDto comment)
+     public async Task<ActionResult<BlogCommentDto>> Update([FromBody] BlogCommentDto blogComment)
     {
-        var result = _blogCommentService.Update(comment, User.PersonId());
-        return CreateResponse(result);
+        string jsonBlogComment = JsonConvert.SerializeObject(blogComment);
+        
+        Uri uri = new Uri("http://localhost:3333/api/blogcomments/{id}");
+        var content = new StringContent(jsonBlogComment);
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        
+        HttpResponseMessage responseMessage = await _httpClient.PutAsync(uri,content);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string responseContent = await responseMessage.Content.ReadAsStringAsync();
+            BlogCommentDto updatedBlogComment = JsonConvert.DeserializeObject<BlogCommentDto>(responseContent);
+            return CreateResponse(Result.Ok(updatedBlogComment));
+        }
+
+        return BadRequest();
     }
 
     [HttpDelete("{id:int}")]
-    public ActionResult Delete(int id)
+     public async Task<ActionResult> Delete(int id)
     {
-        var result = _blogCommentService.Delete(id);
-        return CreateResponse(result);
+        Uri uri = new Uri($"http://localhost:3333/api/blogcomments/{id}");
+        HttpResponseMessage response = await _httpClient.DeleteAsync(uri);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return Ok();
+        }
+        else
+        {
+            return BadRequest();
+        }
     }
 }
