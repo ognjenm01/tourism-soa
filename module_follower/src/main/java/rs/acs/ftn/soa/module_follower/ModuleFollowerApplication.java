abbddc85de880546b82659e1f3ea1cbb1b6bff66
Ognjen Milojevic <ognjenmilojevic2001@gmail.com>
2024-04-14 22:28:56 +0200

    feat: initial spring project structure

---
+++ module_follower/src/main/java/rs/acs/ftn/soa/module_follower/ModuleFollowerApplication.java
@@ -0,0 +1,13 @@
+package rs.acs.ftn.soa.module_follower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleFollowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleFollowerApplication.class, args);
+	}
+
+}
---
+++ module_follower/src/test/java/rs/acs/ftn/soa/module_follower/ModuleFollowerApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.acs.ftn.soa.module_follower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleFollowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
